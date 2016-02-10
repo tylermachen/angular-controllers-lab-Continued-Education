a20af8abf79373be33f2681f813fe389d4252942
Todd Motto <todd@toddmotto.com>
2016-02-10 15:05:31 +0000

    chore: Added tests

---
+++ tests/ContactController.spec.js
@@ -0,0 +1,25 @@
+describe('MainController', function () {
+	var $controller;
+
+	beforeEach(module('app'));
+
+	beforeEach(inject(function (_$controller_) {
+		$controller = _$controller_;
+	}));
+
+
+	it('should not be using $scope to set values', function () {
+		var $scope = {};
+		$controller('ContactController as vm', {$scope: $scope});
+
+		expect($scope.name).toBe(undefined);
+		expect($scope.email).toBe(undefined);
+	});
+	it('should have values assigned to `this`', function () {
+		var $scope = {};
+		$controller('ContactController as vm', {$scope: $scope});
+
+		expect($scope.vm.name).toBe('Bill Gates');
+		expect($scope.vm.email).toBe('william@microsoft.com');
+	})
+});
