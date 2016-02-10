c3d6e97549f8bba62ca8536eb6649889a00a975e
Todd Motto <todd@toddmotto.com>
2016-02-10 15:07:07 +0000

    chore: Added solution

--- js/app/controllers/ContactController.js
+++ js/app/controllers/ContactController.js
@@ -1,6 +1,6 @@
-function ContactController($scope) {
-    $scope.name = 'Bill Gates';
-    $scope.email = 'william@microsoft.com'
+function ContactController() {
+    this.name = 'Bill Gates';
+    this.email = 'william@microsoft.com'
 }
 
 angular
--- js/app/controllers/StaffController.js
+++ js/app/controllers/StaffController.js
@@ -1,6 +1,6 @@
-function StaffController($scope) {
-    $scope.name = 'Steve Jobs';
-    $scope.email = 'steve@apple.com';
+function StaffController() {
+    this.name = 'Steve Jobs';
+    this.email = 'steve@apple.com';
 }
 
 angular
